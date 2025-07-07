f0e025b796e6dcd5a784d441eca9247b3a64f97f
Kevin Kamau <Kamaukevin871@gmail.com>
2025-07-07 14:07:38 +0300

    Refactor comments and remove unnecessary host variable in server setup

--- server/index.ts
+++ server/index.ts
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
+// importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
@@ -58,10 +58,11 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-
+  // ALWAYS serve the app on port 5000
+  // this serves both the API and the client.
+  // It is the only port that is not firewalled.
   const port = 5000;
-  const host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1";
-  server.listen(port, host, () => {
+  server.listen(port, () => {
     log(`serving on port ${port}`);
   });
 })();
